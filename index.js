dd83aef55ce2779f60e8b4336cbb76500df2f179
Jiří Prokop <jprokop@synaptiko.cz>
2017-10-05 13:47:09 +0200

    Added parserOptions

--- index.js
+++ index.js
@@ -7,6 +7,10 @@
 
 module.exports = {
     'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 2017,
+        'sourceType': 'module'
+    },
     'env': {
         'es6': true,
         'node': true,
