40b6c594e113cc7dc60706ce5e2b73db051ba614
Jiří Prokop <jprokop@synaptiko.cz>
2019-01-29 18:59:45 +0100

    AR-1617 Update our rules to include eslint-plugin-bayzat and ember's recommended rules

--- index.js
+++ index.js
@@ -5,26 +5,52 @@
  * @author Piotr Leszkowicz <piotr@bayzat.com>
  * @author Jiri Prokop <jiri@bayzat.com>
  */
+const { DEFAULT_IGNORED_PROPERTIES } = require('eslint-plugin-ember/lib/rules/avoid-leaking-state-in-ember-objects')
 
 module.exports = {
-    'extends': 'eslint:recommended',
-    'parserOptions': {
-        'ecmaVersion': 2017,
-        'sourceType': 'module'
+    plugins: [
+        'ember',
+        'bayzat'
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:ember/recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 2017,
+        sourceType: 'module'
     },
-    'env': {
-        'es6': true,
-        'node': true,
-        'browser': true,
-        'commonjs': true,
-        'mocha': true
+    env: {
+        es6: true,
+        node: true,
+        browser: true,
+        commonjs: true,
+        mocha: true
     },
-    'rules': {
+    rules: {
+        // FIXME jprokop: during implementation of following two rules we discovered
+        // the problem with Ember Data and Ember's proxies; it cannot be safely auto-fixed
+        // We may introduce those rules later as a suggestions, or apply them and revert selectively places
+        // where proxies are used. There is AR-1606 to track this.
+        //
+        // the following two rules can lead to another error:
+        // if the code is autofixed and imported get/getProperties function is not used anymore
+        // => it requires manual change in that case
+        // 'bayzat/no-get-for-flat-props': 'error',
+        // 'bayzat/no-get-properties-for-flat-props': 'error',
+
+        'bayzat/no-get-properties-with-array': 'error',
+        // to support propTypes definition on class prototype
+        'ember/avoid-leaking-state-in-ember-objects': [ 'error', [
+            ...DEFAULT_IGNORED_PROPERTIES,
+            'propTypes'
+        ]],
         'arrow-parens': [ 'error', 'always' ],
         'arrow-body-style': [ 'error', 'always'],
         'arrow-spacing': 'error',
         'brace-style': [ 'error', '1tbs' ],
         'comma-dangle': [ 'error', 'never' ],
+        'comma-spacing': [ 'error', { 'before': false, 'after': true } ],
         'curly': 'error',
         'eqeqeq': 'error',
         'guard-for-in': 'error',
@@ -51,5 +77,18 @@ module.exports = {
         'strict': 'off',
         'quotes': [ 'error', 'single' ],
         'eol-last': [ 'error', 'always' ]
-    }
+    },
+    overrides: [
+        // no jquery in test files
+        {
+            files: [
+                'tests/unit/**/*-test.js',
+                'tests/integration/**/*-test.js',
+                'tests/acceptance/**/*.js'
+            ],
+            rules: {
+                'bayzat/no-jquery': 'error'
+            }
+        }
+    ]
 }
